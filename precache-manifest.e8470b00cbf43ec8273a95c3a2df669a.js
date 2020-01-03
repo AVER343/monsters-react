e37ef84eff25f9449e8d72447694fb438cea7929
AVER343 <ankver343@gmail.com>
2020-01-03 10:45:24 +0530

    Updates

---
+++ precache-manifest.e8470b00cbf43ec8273a95c3a2df669a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31ad1ba3e7c13b8e2e50b999c9b3e78c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "20014ccf99503a67034d",
+    "url": "/monsters-rolodex/static/css/main.2d48764a.chunk.css"
+  },
+  {
+    "revision": "d2c6ca8090bde51ef696",
+    "url": "/monsters-rolodex/static/js/2.c30e0d0f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.c30e0d0f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "20014ccf99503a67034d",
+    "url": "/monsters-rolodex/static/js/main.66d6b025.chunk.js"
+  },
+  {
+    "revision": "8e960e5a979fec14d591",
+    "url": "/monsters-rolodex/static/js/runtime-main.9bdc4c60.js"
+  }
+]);
